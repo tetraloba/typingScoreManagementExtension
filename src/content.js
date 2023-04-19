218568f6e5dbe31ae713eb16ce6a309caf1bec6f
tetraloba <four.leaves.siva7342@gmail.com>
2023-04-19 22:41:08 +0900

    licensed

--- src/content.js
+++ src/content.js
@@ -1,17 +1,9 @@
-// 何かしら、実行のタイミングを制御する記述が必要？ window.onloadは非推奨。
-// manifest.jsonで制御できたりするのかしら。
-
-// var current = document.getElementById("current"); // section id current
-// var resultData = current.getElementsByClassName("result_data")[0]; // div class resultdata
-// var resultList = resultData.firstElementChild; // ul
-
-// let num_childNodes = resultList.childElementCount; // resultListの子要素の数
-// // すべての子要素を配列形式のプロパティで参照
-// for (let i = 0; i < num_childNodes; i++) {
-//     for (let j = 0; j < 2; j++) {
-//         console.log(resultList.children[i].children[j].textContent);
-//     }
-// }
+/**
+ * The program content.js is released under the MIT license.
+ * See https://opensource.org/licenses/MIT
+ *
+ * @author tetraloba
+ */
 
 console.log('test: scriptが実行されました');
 const options = {
